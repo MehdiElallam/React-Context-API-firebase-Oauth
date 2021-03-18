6a2bea38b897aa1ccee266ccf739a48b918a8e9f
Mehdi El <mehdielallam07@gmail.com>
2021-03-18 20:52:16 +0100

    Adding Auth components

---
+++ src/Components/Auth/Signin.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react'
+import { AuthContext } from '../../Firebase/context'
+
+export default function Signin() {
+
+    const { user } = useContext(AuthContext);
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
